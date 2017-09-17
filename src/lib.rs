5e09180a60b9ec275ef99ebc61cc7fa4635fe9b9
coco <coco@hexgolems.com>
2017-09-17 13:26:18 +0200

    initial commit - simple implementation

---
+++ src/lib.rs
@@ -0,0 +1,85 @@
+extern crate rand;
+use rand::Rng;
+
+#[derive(Debug)]
+struct AliasEntry{
+    val: usize,
+    alias: usize,
+    prob_of_val: f64,
+}
+
+impl AliasEntry{
+    pub fn new(val: usize, alias: usize, prob_of_val: f64) -> Self {
+        return AliasEntry{val, alias, prob_of_val}
+    }
+}
+
+#[derive(Debug)]
+pub struct LoadedDiceSampler<R: Rng> {
+    entries: Vec<AliasEntry>,
+    rng: R,
+}
+
+impl<R: Rng> LoadedDiceSampler<R> {
+    pub fn new(probs: Vec<f64>, rng: R) -> Self{
+        let entries = LoadedDiceSampler::<R>::construct_table(probs);
+        Self{entries, rng}
+    }
+
+    pub fn sample(&mut self) -> usize{
+        let index = self.rng.gen_range(0,self.entries.len());
+        let coin = self.rng.next_f64();
+        let entry = &self.entries[index];
+        if coin > entry.prob_of_val  {
+            return entry.alias;
+        }
+        return entry.val;
+    }
+
+    fn construct_table(probs: Vec<f64>) -> Vec<AliasEntry>{
+        let mut res = vec!();
+        let n = probs.len() as f64;
+        let inv_n = 1.0 / probs.len() as f64;
+
+        let mut tmp = {probs.clone().into_iter().enumerate().collect::<Vec<_>>()};
+
+        while tmp.len() > 1{
+            tmp.sort_by(|&(_,p1), &(_,p2)| p2.partial_cmp(&p1).unwrap()); // [biggest-prob, ...,  smallest-prob]
+            let (min_i, min_p) = tmp.pop().unwrap();
+            let &mut (ref max_i, ref mut max_p) = tmp.get_mut(0).unwrap();
+            res.push(AliasEntry::new(min_i, *max_i, min_p*n));
+            let used_prob = inv_n - min_p;
+            *max_p -= used_prob;
+        }
+        let (last_i, last_p) = tmp.pop().unwrap(); 
+        assert!(0.999 < last_p*n && last_p*n < 1.001); // last value should always be exactly 1 but floats...
+        res.push(AliasEntry::new(last_i, std::usize::MAX, 1.0));
+
+        return res;
+    }
+
+
+}
+
+#[cfg(test)]
+mod tests {
+    use ::LoadedDiceSampler;
+    use rand::thread_rng;
+    #[test]
+    fn it_works() {
+        let base = vec!(0.5,0.3,0.1,0.1);
+        let mut s = LoadedDiceSampler::new(base.clone(), thread_rng());
+        let mut res: Vec<usize> = vec!(0; 4);
+        let iter: usize = 1000000;
+        for i in (0..iter) {
+            let i = s.sample();
+            res[i] += 1;
+        }
+        let res_p = res.iter().map(|&f| f as f64 / iter as f64).collect::<Vec<_>>();
+        println!("{:?}", res_p);
+        for (i,c) in res.iter().enumerate(){
+            let p_i = *c as f64 / iter as f64;
+            assert!(base[i] *0.99 < p_i && base[i] * 1.01 > p_i);
+        }
+    }
+}
