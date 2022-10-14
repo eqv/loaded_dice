e399e4634e2873013d152a0ae54465d9d2e325dd
Dominik Maier <dmnk@google.com>
2022-10-14 17:26:17 +0200

    Update rand, clippy

--- src/example.rs
+++ src/example.rs
@@ -2,11 +2,11 @@ extern crate loaded_dice;
 extern crate rand;
 
 use loaded_dice::LoadedDiceSampler;
-use rand::{thread_rng, Rng};
+use rand::thread_rng;
 fn main() {
-    let mut s = LoadedDiceSampler::new(vec!(0.5, 0.3, 0.1, 0.1), thread_rng());
+    let mut s = LoadedDiceSampler::new(vec![0.5, 0.3, 0.1, 0.1], thread_rng());
     let iter: usize = 100;
-    for i in (0..iter) {
+    for _ in 0..iter {
         println!("{}", s.sample());
     }
 }
